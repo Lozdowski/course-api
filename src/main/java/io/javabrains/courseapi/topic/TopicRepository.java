00b0d40836559546af66e1ab8b8216ebccefbca4
Piotr Lozdowski <lozdowski.piotr@gmail.com>
2019-11-11 23:35:38 +0100

    first commit for dataJPA

---
+++ src/main/java/io/javabrains/courseapi/topic/TopicRepository.java
@@ -0,0 +1,16 @@
+package io.javabrains.courseapi.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String> {
+    //getAllTopics()
+    //getTopic(String id)
+    //updateTopic(Topic t)
+    //deleteTopic(String)
+    // dont need it since it is already in library
+
+
+
+
+
+}
